07fd9a4534ab77cfa1056e3ae3fb3e37e4425dff
Emmanuel Adeleke <caesaradek@gmail.com>
2026-01-17 10:53:44 +0000

    Add healthcheck handler and update main.go for server configuration

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,30 +4,31 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"os"
 	"net/http"
+	"os"
+	"time"
 )
 
+// fmt.Println("Hello, World!")
+const version = "1.0.0"
 
-	// fmt.Println("Hello, World!")
-	const version = "1.0.0"
-	// fmt.Println("Version:", version)
+// fmt.Println("Version:", version)
 
-	type config struct {
-		port int
-		env  string
-	}
+type config struct {
+	port int
+	env  string
+}
 
-	type application struct {
-		config config
-		logger *log.Logger
-	}
+type application struct {
+	config config
+	logger *log.Logger
+}
 
 func main() {
 	var cfg config
 
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
-	flag.StringVar((&cfg.env, "env", "development", "Environment (development|staging|production)"))
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
 	flag.Parse()
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
@@ -42,7 +43,15 @@ func main() {
 	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
 
 	srv := &http.Server{
-		Addr:  fmt.Sprintf(":%d", cfg.port),
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      mux,
+		IdleTimeout:  time.Second,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
 	}
+
+	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
+	err := srv.ListenAndServe()
+	logger.Fatal(err)
 
 }

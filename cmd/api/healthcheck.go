be5f2286a5b156ef750dbea31310eb4c66744a75
Emmanuel Adeleke <caesaradek@gmail.com>
2026-01-25 13:27:18 +0000

    feat: update healthcheck handler to return JSON response and add readIDParams helper function

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -6,7 +6,14 @@ import (
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	// fmt.Fprintln(w, "status: available")
+	// fmt.Fprintf(w, "environment: %s\n", app.config.env)
+	// fmt.Fprintf(w, "version: %s\n", version)
+
+	jsonResponse := `{ "status": "available", "environment": %q, "version": %q}`
+	jsonResponse = fmt.Sprintf(jsonResponse, app.config.env, version)
+
+	w.Header().Set("Content-Type", "application/json")
+
+	w.Write([]byte(jsonResponse))
 }

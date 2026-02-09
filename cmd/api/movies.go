7c59f37e706816ab86fa4be7a03390d88768615d
Emmanuel Adeleke <caesaradek@gmail.com>
2026-02-09 07:52:53 +0100

    feat: implement movie data structure and update showMovieHandler to return movie details as JSON

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"caesar.emmanuel.net/internal/data"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -17,6 +20,16 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		http.NotFound(w, r)
 		return
 	}
-
 	fmt.Fprintf(w, "this is the movie id boss %d\n", movieId)
+
+	movieData := data.Movie{ID: movieId, CreatedAt: time.Now(), Title: "Emmanuel",
+		Runtime: 102, Genres: []string{"thriller", "crime", " romance"}}
+
+	err = app.writeJSON(w, http.StatusOK, movieData, nil)
+
+	if err != nil {
+		app.logger.Print(err)
+		http.Error(w, "The ser er has an error", http.StatusInternalServerError)
+	}
+
 }
